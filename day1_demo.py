6b13740a440d572ed497d63dd21bd630fe05eb1d
Kenji1245 <116498504+Kenji1245@users.noreply.github.com>
2024-11-08 12:22:33 +0000

    Add files via upload

---
+++ day1_demo.py
@@ -0,0 +1,5 @@
+city = input("What's the name of the city you grew up in? : ")
+print(city)
+pet = input("What's your pet's name? : ")
+print(pet)
+print("Your band name could be " + city +" "+ pet)
